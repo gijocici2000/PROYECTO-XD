1f5ac4d8fb83708f2d964a3064f2404f6fccda76
gijocici2000 <149328695+gijocici2000@users.noreply.github.com>
2025-07-16 19:04:43 -0500

    Actualizar form.py

--- compras/form.py
+++ compras/form.py
@@ -35,3 +35,39 @@ class DetalleCompraForm(forms.Form):
         decimal_places=2,
         widget=forms.NumberInput(attrs={'class': 'form-control'})
     )
+
+
+class BuscadorCompraForm(forms.Form):
+    proveedor = forms.ModelChoiceField(
+        queryset=Proveedor.objects.all(),
+        label='Proveedor',
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    numero_factura = forms.CharField(
+        label='Número de Factura',
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Buscar por N° de Factura'
+        })
+    )
+
+    fecha_inicio = forms.DateField(
+        label='Desde',
+        required=False,
+        widget=forms.DateInput(attrs={
+            'class': 'form-control',
+            'type': 'date'
+        })
+    )
+
+    fecha_fin = forms.DateField(
+        label='Hasta',
+        required=False,
+        widget=forms.DateInput(attrs={
+            'class': 'form-control',
+            'type': 'date'
+        })
+    )
